872731e3d6ae8775c6c4d4f9ab76a56d1e424a53
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-06-29 19:00:10 +0530

    gitignore issue

--- 01. Basic IOs, Syntax, Datatypes and Variables/01_use_of_cin_dot_get.cpp
+++ 01. Basic IOs, Syntax, Datatypes and Variables/01_use_of_cin_dot_get.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-    /*          USE OF CIN.GET()
-         The command "cin.get()" is used to get the ASCII value of any type of characters
-     including tab,space,enter,numbers,alphabets and all those mentioned in ASCII Table
-     */
+/*          USE OF CIN.GET()
+        
+	cin ignores ENTER(\n), TAB(\t) and SPACE( ) while taking input. These are called Whitespace Characters.
+	We use cin.get() to read these whitespace characters
+*/
+     
     int a;
     cout << "Hey there ! Input any character and hit enter" << endl;
     //"Enter" is also a character

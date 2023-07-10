2e55d8a562e104c48be696e31a2706f5f9e6d6e0
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-07-10 20:15:45 +0530

    commit

---
+++ Test Codes/01_reverse_an_integer.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the integer value to be reversed:"<<endl;
+    cin>>n;
+    int ans = 0;
+    while(n!=0)
+    {
+        int digit = n % 10;
+        ans = ( ans * 10 ) + digit;
+        n = n / 10;
+
+    }
+
+    if((ans>-pow(2,31))&&(ans<(pow(2,31)-1)))
+    {
+        cout<<ans;
+    }
+   
+    return 0;
+}

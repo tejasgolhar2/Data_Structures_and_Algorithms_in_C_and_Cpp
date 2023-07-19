ccb89720e12872058ec34fa211148f74d51cb9d6
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-07-19 13:36:14 +0530

    commit

--- 04. Operators/03_logical_operators.c
+++ 04. Operators/03_logical_operators.c
@@ -4,7 +4,13 @@
 
 int main()
 {
-    int a =5;
-    printf("%d",!a);   
+    /*int a =5;
+    printf("%d",!a); */  
+    int a=1;
+    int b = 6;
+    int result1 = a-- && ++b;  
+    printf("%d\n",result1);
+    int result2 = --a && ++b;
+    printf("%d",result2);
     return 0;
 }

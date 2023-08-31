f28d5534348fd8b5d9ccad5f9b15a82d8344c4aa
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-08-31 19:31:36 +0530

    commit

---
+++ 08. Arrays/Data Structure Operations/Searching/01_binary_search.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int size;
+    cout<<"Enter the size of the array:0"<<endl;
+    int key, start = 0, end = size -1;
+    cout<<"Enter the value to be searched in the array: "<<endl;
+    cin>>key;
+
+
+
+
+    cin>>
+    return 0;
+}

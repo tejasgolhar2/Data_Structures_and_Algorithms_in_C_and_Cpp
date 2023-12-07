cdda6fea573962972a7b6e9d0b0ee72e2f8f2fd3
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-12-07 09:17:16 +0530

    commit

--- Programming Problems/01_pr.cpp
+++ Programming Problems/01_pr.cpp
@@ -4,15 +4,25 @@ using namespace std;
 
 int main()
 {
-    int num;
+    int n;
     cout<<"Enter number: ";
-    cin>>num;
+    cin>>n;
 
-    for(int i=num;i>0;i--)
+    for(int i=0;i<n;i++)
     {
-        for(int j=i;j>0;j--)
+        for(int j=0;j<(n-i-1);j++)
+        {
+            cout<<" ";
+        }
+
+        for(int j=0;j<(2*i+1);j++)
         {
             cout<<"*";
+        }
+
+        for(int j=0;j<(n-i-1);j++)
+        {
+            cout<<" ";
         }
         cout<<"\n";
     }
